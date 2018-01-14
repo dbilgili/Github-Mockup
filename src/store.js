a59d81bca18a0a3ce27a8356c9bf78265d41c478
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2018-01-14 16:47:40 +0100

    delete middleware

--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import {createStore, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import allReducers from './reducers/index'
 
-const store = createStore(allReducers,{},applyMiddleware(createLogger()));
+const store = createStore(allReducers,{});
 
 export default store;
