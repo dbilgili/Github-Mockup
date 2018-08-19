92169ee83b3a7bb5f07fef837d96cc5c62b6a436
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2018-08-19 18:50:56 +0200

    Switch back to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {HashRouter, Route} from 'react-router-dom'
 import AllRoutes from './components/allRoutes'
 import './styles/main.css'
 import './styles/responsive.css'
@@ -9,9 +9,9 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Route component={AllRoutes}/>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
